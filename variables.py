a6f292a710cf07ef012d7a0ab143b0c8ed24da38
Jason Kwong <jasonkwong@jason-kwongs-macbook.local>
2023-08-20 20:16:31 -0400

    add files

---
+++ variables.py
@@ -0,0 +1,6 @@
+S3_BUCKET_NAME = 'dreambooth-stable-diffusion-bucket'
+S3_BUCKET_MODELS_PREFIX = 'out/models'
+S3_BUCKET_IMGS_PREFIX = 'out/imgs'
+S3_BUCKET_TRAINING_DATA_PREFIX = 'training_data'
+
+SQS_QUEUE_URL = "https://sqs.us-east-1.amazonaws.com/489671656010/DreamboothQueue.fifo"
