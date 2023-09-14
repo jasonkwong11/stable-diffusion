b1cc7575c9504c066cd1e416e5eca23b606bb88d
Jason Kwong <jason.d.kwong@gmail.com>
2023-09-14 13:48:34 -0400

    update

--- prompts.py
+++ prompts.py
@@ -12,16 +12,16 @@ DEFAULT_PROMPTS = {
                 'background, art by wlop, greg rutkowski, thierry doizon, charlie bowater, '
                 'alphonse mucha, golden hour lighting, ultra realistic.',
         'Nerd': 'Closeup portrait of @me as a software engineer computer nerd.',
-        'Woman': 'closeup portrait of @me as a female woman.',
-        'Self': 'closeup portrait of @me.',
-        'Ren': 'closeup portrait of @me as a renaissance painting.',
+        'Woman': 'closeup portrait of @me as a female woman, intricate details, smooth lighting.',
+        'Self': 'closeup portrait of @me, intricate details, smooth lighting.',
+        'Ren': 'closeup portrait of @me as a renaissance painting, intricate details, smooth lighting.',
         'Elf': 'Closeup portrait of @me as an elf with long blond hair, fantasy concept art, intricate '
                 'details, detailed armor, majestic background, art by wlop, Greg Rutkowski, digital '
                 'painting, smooth lighting, looking towards the viewer.',
-        'Soccer': 'closeup portrait of @me as a soccer player @me wearing a xred and white uniform, '
-                'action shot, grassy field in the background, bright sunlight, motion blur, dramatic '
-                'lighting, intense facial expression, art by ross tran, charlie bowater, ignacio '
-                'fernandez rios, kai carpenter, leesha hannigan, thierry doizon.',
+        # 'Soccer': 'closeup portrait of @me as a soccer player @me wearing a xred and white uniform, '
+        #         'action shot, grassy field in the background, bright sunlight, motion blur, dramatic '
+        #         'lighting, intense facial expression, art by ross tran, charlie bowater, ignacio '
+        #         'fernandez rios, kai carpenter, leesha hannigan, thierry doizon.',
         'Clown': 'Closeup portrait of @me as a clown, highly detailed, surreal, expressionless face, '
                 'bright colors, contrast lighting, abstract background, art by wlop, greg rutkowski, '
                 'charlie bowater, magali villeneuve, alphonse mucha, cartoonish, comic book style.',
@@ -63,17 +63,17 @@ DEFAULT_PROMPTS = {
                 'intricate details, eerie, highly detailed, photorealistic, 8k, unreal engine. art by '
                 'artgerm and greg rutkowski and charlie bowater and magali villeneuve and alphonse mucha, '
                 'golden hour, cyberpunk, robotic, steampunk, neon colors, metallic textures.',
-        'Monster': 'closeup portrait of @me as monster with glowing eyes and sharp teeth, dark shadows, '
-                'foggy background, highly detailed, photorealism, concept art, digital painting, art '
-                'by yahoo kim, max grecke, james white, fabrizio bortolussi.',
-        'Vampire': 'closeup portrait of @me as a vampire, pale skin, dark eyes, sharp fangs, detailed '
-                'shadows and highlights, eerie atmosphere, mystical and magical, art by leesha hannigan, '
-                'thierry doizon, alphonse mucha, kai carpenter, noir photorealism, surreal and '
-                'dreamlike, deep red hues.',
-        'Zombie': 'closeup portrait of @me as a zombie, decaying skin and clothing, dark and eerie, highly '
-                'detailed, photorealistic, 8k, ultra realistic, horror style, art by greg rutkowski, '
-                'charlie bowater, and magali villeneuve.',
-        'Witch': 'closeup portrait of @me as a witch surrounded by magical elements, highly detailed, '
-                'photorealism, digital painting, dark colors, grayscale, intricate details, art by yuumei, '
-                'greg rutkowski, eddie hong, and charlie bowater, ultra realism, magical elements.'
+        # 'Monster': 'closeup portrait of @me as monster with glowing eyes and sharp teeth, dark shadows, '
+        #         'foggy background, highly detailed, photorealism, concept art, digital painting, art '
+        #         'by yahoo kim, max grecke, james white, fabrizio bortolussi.',
+        # 'Vampire': 'closeup portrait of @me as a vampire, pale skin, dark eyes, sharp fangs, detailed '
+        #         'shadows and highlights, eerie atmosphere, mystical and magical, art by leesha hannigan, '
+        #         'thierry doizon, alphonse mucha, kai carpenter, noir photorealism, surreal and '
+        #         'dreamlike, deep red hues.',
+        # 'Zombie': 'closeup portrait of @me as a zombie, decaying skin and clothing, dark and eerie, highly '
+        #         'detailed, photorealistic, 8k, ultra realistic, horror style, art by greg rutkowski, '
+        #         'charlie bowater, and magali villeneuve.',
+        # 'Witch': 'closeup portrait of @me as a witch surrounded by magical elements, highly detailed, '
+        #         'photorealism, digital painting, dark colors, grayscale, intricate details, art by yuumei, '
+        #         'greg rutkowski, eddie hong, and charlie bowater, ultra realism, magical elements.'
 }
